7b5172ad3fd9c65393f7e54368c18e7634ea53c1
Miles Friedman <milesfriedmanfl@gmail.com>
2014-05-13 20:11:03 -0700

    Initial Commit.

---
+++ src/bracketGame/Parser.java
@@ -0,0 +1,19 @@
+package bracketGame;
+
+/**
+ * Parser.java
+ * 
+ * @author Miles
+ * @version 5/12/14
+ */
+public class Parser {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
